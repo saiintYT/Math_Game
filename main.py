eb6ba0cb970a4f3f19bc49a6fc502b6fdf7980e1
saiintYT <131825811+saiintYT@users.noreply.github.com>
2023-08-29 23:00:09 +0000

    Added player end screen.

--- main.py
+++ main.py
@@ -1,59 +1,63 @@
 import random
 import time
 
+
 def starting_menu():
+  print("Welcome to the Math Game!")
+  print("You will be presented with addition and subtraction questions.")
+  print("You have 3 lives.")
+  print("Good luck!\n")
 
-  # Nate Lepper
-  # August 22 2023
-  # Starting Menu
-  # Version 1
-  
-    print("Welcome to the Math Game!")
-    print("You will be presented with addition and subtraction questions.")
-  
+
 def generate_question():
-  # Nate Lepper
-  # August 21 2023
-  # Random Question Generate
-  # Version 1
-    num1 = random.randint(1, 20)
-    num2 = random.randint(1, 20)
-    operator = random.choice(['+', '-'])
-    if operator == '-':
-        if num1 < num2:
-            num1, num2 = num2, num1  # Swap numbers to avoid negative results
-    question = f"What is {num1} {operator} {num2}? "
-    answer = num1 + num2 if operator == '+' else num1 - num2
-    return question, answer
+  num1 = random.randint(1, 50)
+  num2 = random.randint(1, 50)
+  operator = random.choice(['+', '-'])
 
-score = 0
-# Set Score
-num_questions = 5
-# Set Number of Questions
+  if operator == '-' and num1 < num2:
+    num1, num2 = num2, num1  # Swap numbers to avoid negative results
 
-  # Main Sequence
-starting_menu()
-for _ in range(num_questions):
-  # Nate Lepper
-  # August 21 2023
-  # Printing Number Generation on Console
-  # Version 2
-  question, correct_answer = generate_question()
-  print(question)
-  
-  user_answer = input("Your answer: ")
+  question = f"What is {num1} {operator} {num2}? "
+  answer = num1 + num2 if operator == '+' else num1 - num2
+  return question, answer
 
-  try:
-  # Nate Lepper
-  # August 22 2023
-  # Getting User Input and Checking User Input
-  # Version 2
-      if int(user_answer) == correct_answer:
+
+def main():
+  score = 0
+  num_questions = 5
+  lives = 3
+
+  starting_menu()
+
+  playing = True
+  while playing:
+    for _ in range(num_questions):
+      question, correct_answer = generate_question()
+      print(question)
+
+      user_answer = input("Your answer: ")
+
+      try:
+        if int(user_answer) == correct_answer:
           print("Correct!\n")
           score += 1
-      else:
+        else:
           print(f"Wrong! The correct answer is {correct_answer}\n")
-  except ValueError:
-      print("Invalid input. Please enter a valid number.\n")
+      except ValueError:
+        print("Invalid input. Please enter a valid number.\n")
 
-print(f"Game over! Your score: {score}/{num_questions}")
+    print(f"Game over! Your score: {score}/{num_questions}")
+    play_again_input = input("Do you want to play again? (yes/no): ")
+    play_again = play_again_input.lower() == 'yes'
+
+    if play_again:
+      print("\nStarting a new game...\n")
+      score = 0
+      lives = 3
+    else:
+      print("Thank you for playing!")
+      playing = False
+
+
+if __name__ == "__main__":
+  main()
